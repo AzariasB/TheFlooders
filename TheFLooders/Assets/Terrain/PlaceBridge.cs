8c18992865d73b4bae74985f88207180727cae23
AzariasB <azarias.boutin@gmail.com>
2017-03-18 12:57:42 +0100

    Rotating bridge

---
+++ TheFLooders/Assets/Terrain/PlaceBridge.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceBridge : MonoBehaviour {
+
+    bool Placed { get; set; }
+    bool Rotated { get; set; }
+
+	// Use this for initialization
+	void Start () {
+        Placed = false;
+        Rotated = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!Placed)
+        {
+            bool rayCast;
+            RaycastHit hitPoint;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            rayCast = Physics.Raycast(ray, out hitPoint, 100.0f);
+            if (Input.GetMouseButtonDown(0))
+            {
+                Placed = rayCast;
+                //Change layer
+                gameObject.layer = LayerMask.NameToLayer("Default");
+            }
+            else
+            {
+                transform.position = (hitPoint.point + new Vector3(0 , 1, 0) );
+            }
+
+        }else if(!Rotated)
+        {
+            float rotation = Input.GetAxis("Mouse ScrollWheel");
+            gameObject.transform.Rotate(Vector3.up * rotation * 8192 * Time.deltaTime );
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                Rotated = true;
+            }
+
+        }
+    }
+}
--- TheFLooders/Assets/UI/AddBridge.cs
+++ TheFLooders/Assets/UI/AddBridge.cs
@@ -1,16 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AddBridge : MonoBehaviour {
 
+
 	// Use this for initialization
 	void Start () {
-		
+        Button button = GetComponent<Button>();
+        button.onClick.AddListener(AddBrigeTask);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void AddBrigeTask()
+    {
+        //Check if remain enough bridges
+        GameObject nwBrige = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        //Change size ...
+
+        nwBrige.layer = LayerMask.NameToLayer("Ignore Raycast");
+        nwBrige.AddComponent<PlaceBridge>();
+    }
 }

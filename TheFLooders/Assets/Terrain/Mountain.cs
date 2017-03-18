36080a927a40d4a6f7db22aaddad06f971a640a8
AzariasB <azarias.boutin@gmail.com>
2017-03-18 12:28:56 +0100

    Added "add mountain" button an logic

---
+++ TheFLooders/Assets/Terrain/Mountain.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Quand est ajouté à un GameObject, va suivre
+/// la souris, jusqu'au clic de celle-ci, quan cliqué,
+/// va poser l'objet en question
+/// </summary>
+public class Mountain : MonoBehaviour {
+
+    public bool Placed { get; set; }
+
+	// Use this for initialization
+	void Start () {
+        Placed = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (!Placed)
+        {
+            bool rayCast;
+            RaycastHit hitPoint;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            rayCast = Physics.Raycast(ray, out hitPoint, 100.0f);
+            if (Input.GetMouseButtonDown(0))
+            {
+                Placed = rayCast;
+                //Change layer
+                gameObject.layer = LayerMask.NameToLayer("Default");
+            }else
+            {
+                transform.position = hitPoint.point;
+            }
+
+        }
+    }
+}
---
+++ TheFLooders/Assets/UI/AddMountain.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddMountain : MonoBehaviour {
+
+
+    Button _button;
+	// Use this for initialization
+	void Start () {
+        _button = GetComponent<Button>();
+        _button.onClick.AddListener(AddMountainTask);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void AddMountainTask()
+    {
+        print("Adding object");
+        GameObject nwObj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        nwObj.transform.position = new Vector3(0, 10, 0);
+        //nwObj.AddComponent<Rigidbody>();
+        nwObj.layer = LayerMask.NameToLayer("Ignore Raycast");
+        nwObj.AddComponent<Mountain>();
+    }
+}

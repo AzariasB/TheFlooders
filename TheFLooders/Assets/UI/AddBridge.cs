78227de74907e55641f881761012623e5303c3b8
AzariasB <azarias.boutin@gmail.com>
2017-03-18 16:14:38 +0100

    Added material to bridge

--- TheFLooders/Assets/UI/AddBridge.cs
+++ TheFLooders/Assets/UI/AddBridge.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class AddBridge : MonoBehaviour {
 
 
+    private Vector3 bridgeSize = new Vector3(6, 1, 2);
+
 	// Use this for initialization
 	void Start () {
         Button button = GetComponent<Button>();
@@ -23,7 +25,13 @@ public class AddBridge : MonoBehaviour {
         if((GameObject.Find("bridge_text").GetComponent<TextBinding>() as TextBinding).CanDecrement())
         {
             GameObject nwBrige = GameObject.CreatePrimitive(PrimitiveType.Plane);
+
+            //Set material
+            Material bridgeMat = Resources.Load("Terrain/Bridge_Material", typeof(Material)) as Material;
+            nwBrige.GetComponent<Renderer>().material = bridgeMat;
+
             //Change size ...
+            nwBrige.transform.localScale = bridgeSize;
 
             nwBrige.layer = LayerMask.NameToLayer("Ignore Raycast");
             nwBrige.AddComponent<PlaceBridge>();

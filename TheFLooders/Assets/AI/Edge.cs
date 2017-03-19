0d314046311a898bb0d93e7153feb2220c0ccc88
AzariasB <azarias.boutin@gmail.com>
2017-03-19 03:23:57 +0100

    Started AI

---
+++ TheFLooders/Assets/AI/Edge.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Edge of non-oriented graph
+/// 1-----2
+/// </summary>
+public class Edge {
+
+    private GraphNode _node1;
+    private GraphNode _node2;
+
+    /// <summary>
+    /// Cost of this edge, might be useless
+    /// </summary>
+    public int Cost { get; set; }
+
+    public Edge(GraphNode node1 = null, GraphNode node2 = null)
+    {
+        _node1 = node1;
+        _node2 = node2;
+    }
+
+    
+
+}
---
+++ TheFLooders/Assets/AI/GraphNode.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraphNode {
+
+	public bool Sinked { get; set; }
+
+    public Vector3 Position;
+
+
+}

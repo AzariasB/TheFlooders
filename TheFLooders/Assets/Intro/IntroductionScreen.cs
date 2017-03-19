4f88d32203643bdab87e92bfac81e232236621de
Yves Grasland <graslany@gmx.com>
2017-03-19 11:33:19 +0100

    Script de la scène d'intro

---
+++ TheFLooders/Assets/Intro/IntroductionScreen.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroductionScreen : MonoBehaviour {
+
+    private MovieTexture _movieTex;
+
+	void Start () {
+        MeshRenderer mr = GetComponent<MeshRenderer> ();
+        Material mat = (mr != null ? mr.material : null);
+        _movieTex = (mat != null ? mat.mainTexture as MovieTexture : null);
+
+        if (_movieTex == null) {
+            Debug.LogError ("Vidéo à lancer non trouvée");
+            return;
+        }
+        _movieTex.Play();
+	}
+	
+    // Change de scène une fois que la vidéo est terminée.
+	void Update () {
+        if (!_movieTex.isPlaying) {
+            SceneManager.LoadScene(1);
+        }
+	}
+}

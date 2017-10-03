cfd7b8d4e6d4c7ea475ee2fc1028146cffd7d02d
Yves Grasland <yves.grasland@gmx.fr>
2017-10-03 20:46:49 +0200

    - Rétablissement d'une intro plus ou moins fonctionnelle (la vidéo lag...) avec le son + gestion de la touche Escape pour quitter l'intro. - Ecrasement de Level1.unity par Yves Sandobx.unity + mise à jour des parmaètres de build (Yves Sandbox n'est par déifnition pas faite pour aller dans les build)

--- TheFlooders/Assets/Intro/IntroductionScreen.cs
+++ TheFlooders/Assets/Intro/IntroductionScreen.cs
@@ -2,35 +2,78 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class IntroductionScreen : MonoBehaviour
 {
+	/// <summary>
+	/// Le VideoPlayer qui joue l'introduction. Apparemment, jouer le son avec ne fonctionne
+	/// pas (?), donc on a un lecteur audio à part qu'il faut synchroniser avec.
+	/// </summary>
+	[SerializeField]
+	private VideoPlayer introVideoPlayer;
 
-    private MovieTexture _movieTex;
+	/// <summary>
+	/// Le lecteur audio qui joue la bande sonore de la vidéo d'intro.
+	/// </summary>
+	[SerializeField]
+	private AudioSource introMusicPlayer;
+
+	/// <summary>
+	/// Vrai si la vidéo d'introduction a été lancée avec succès.
+	/// </summary>
+	private bool startedPlayingVideo;
 
     void Start()
-    {
-        MeshRenderer mr = GetComponent<MeshRenderer>();
-        Material mat = (mr != null ? mr.material : null);
-        _movieTex = (mat != null ? mat.mainTexture as MovieTexture : null);
+	{
+		if (introVideoPlayer == null)
+			introVideoPlayer = GetComponent<VideoPlayer> ();
+		if (introMusicPlayer == null)
+			introMusicPlayer = GetComponent<AudioSource> ();
 
-        if (_movieTex != null)
-        {
-            _movieTex.Play();
-        }
-        else
-        {
-            Debug.LogError("Vidéo à lancer non trouvée");
-            return;
-        }
+		if (introVideoPlayer != null) {
+			PrepareVideoPlayer ();
+		}
     }
 	
     // Change de scène une fois que la vidéo est terminée.
-    void Update()
+    protected virtual void Update()
     {
-        if (_movieTex == null || !_movieTex.isPlaying)
-        {
+		if (introVideoPlayer != null && !Input.GetKeyDown(KeyCode.Escape))
+		{
+			// Si la vidéo a démarré et s'est terminée, passer à la scène suivante
+			if (startedPlayingVideo && !introVideoPlayer.isPlaying)
+				SceneManager.LoadScene(1);
+		}
+		else
+			// Pas de vidéo ou Escape appuyée => Passage à la scène suivante.
             SceneManager.LoadScene(1);
-        }
     }
+
+	/// <summary>
+	/// Lance le préchargement de la vidéo d'intro.
+	/// </summary>
+	private void PrepareVideoPlayer() {
+		introVideoPlayer.prepareCompleted += OnVideoPlayerPrepareCompleted;
+		introVideoPlayer.errorReceived += OnVideoPlayerErrorReceived;
+		introVideoPlayer.Prepare();
+	}
+
+	private void OnVideoPlayerPrepareCompleted(VideoPlayer source) {
+		RemoveVideoPlayerObservers();
+		startedPlayingVideo = true;
+		introVideoPlayer.Play();
+		if(introMusicPlayer != null)
+			introMusicPlayer.Play();
+	}
+
+	private void OnVideoPlayerErrorReceived(VideoPlayer source, string message) {
+		RemoveVideoPlayerObservers();
+		Debug.LogError("Echec du chargement de la vidéo d'introduction : " + (message ?? "<null>"));
+	}
+
+	private void RemoveVideoPlayerObservers() {
+		introVideoPlayer.prepareCompleted -= OnVideoPlayerPrepareCompleted;
+		introVideoPlayer.errorReceived -= OnVideoPlayerErrorReceived;
+	}
 }
